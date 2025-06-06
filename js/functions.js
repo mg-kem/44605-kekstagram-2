9762b0be5b655627c9460b76534cf6a51b766436
Дмитрий Гилёв <kem-mit@yandex.ru>
2025-06-06 21:29:20 +0700

    Подправил ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,33 @@
-// Функция для проверки длины строки
-const checkLenghtSting = (string = "", maxSimbols = 1) =>
-  string.lenght <= maxSimbols;
+// Проверка длины строки
+const checkLengthString = (string = '', lengthString = 1) =>
+  string.length <= lengthString;
 
-checkLenghtSting("проверяемая строка", 20);
+checkLengthString('Проверка строки', 16);
 
-// Проверка на палиндром
-const isPalindrom = (string) {
-  string = string.replaseAll(' ', '');
-  string = string.toLowerCase();
+// Проверка строки на палиндром
+const isPalindrom = (string = '') => {
+  string = string.replaceAll(' ', '').toLowerCase();
 
-  let reversed ='';
-
-  for (i = string.lenght; i < string.lenght - 1; i-- ) {
-    reversed += string[i];
+  let reverseString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverseString += string[i];
   }
-
-  return string === reversed;
+  return reverseString === string;
 };
+
+isPalindrom('Топот');
+
+// Делаем строку положительным числом
+
+const makeStringNumber = (string = '') => {
+  let stringOfNumbers = 'Пустая строка';
+  for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+    if (Number.isNaN(string[i])) {
+      stringOfNumbers += string[i];
+    }
+  }
+  return stringOfNumbers;
+};
+
+makeStringNumber('23rwesdv-1');
